fe0e5c3ad3da9ba5207923780159bfdbfec434ee
admaxpanda <admaxpanda@Outlook.com>
2019-11-27 20:36:56 +0800

    2019.11.27

--- C/实验6/2019.11.27_4.c
+++ C/实验6/2019.11.27_4.c
@@ -5,18 +5,26 @@ int main()
     int bai,wushi,shi,wu,yi,wujiao,yijiao;
     scanf("%f",&qwq);
     printf("%.1f元=",qwq);
-    printf("%d张壹佰元",(int)qwq/100);
-    qwq-=qwq/100*100;
-    printf("%d张伍拾元",(int)qwq/50);
-    qwq-=qwq/50*50;
-    printf("%d张壹拾元",qwq/10);
-    qwq-=qwq/10*10;
-    printf("%d张伍元",qwq/5);
-    qwq-=qwq/5*5;
-    printf("%d张壹元",qwq/1);
-    qwq-=qwq/1;
-    printf("%d张伍角",qwq/0.5);
-    qwq-=qwq/0.5*0.5;
-    printf("%d张壹角",qwq/0.1);
+    if((int)(qwq/100))
+    printf("%d张壹佰元",(int)(qwq/100));
+    qwq-=(int)qwq/100*100;
+    //printf("\n%f\n",qwq);
+    if((int)(qwq/50))
+    printf("+%d张伍拾元",(int)(qwq/50));
+    qwq-=(int)qwq/50*50;
+    if((int)(qwq/10))
+    printf("+%d张壹拾元",(int)(qwq/10));
+    qwq-=(int)qwq/10*10;
+    if((int)(qwq/5))
+    printf("+%d张伍元",(int)(qwq/5));
+    qwq-=(int)qwq/5*5;
+    if((int)(qwq/1))
+    printf("+%d张壹元",(int)(qwq/1));
+    qwq-=(int)qwq/1;
+    if((qwq/0.5))
+    printf("+%d张伍角",(int)(qwq/0.5));
+    qwq-=((int)(qwq/0.5))*0.5;
+    if((qwq/0.1))
+    printf("+%d张壹角",(int)(qwq/0.1));
     printf("\n");
 }
